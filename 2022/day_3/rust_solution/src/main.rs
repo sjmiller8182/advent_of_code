61330c7c199eb0ead0871556ef670d5551add218
Stuart Miller <stuart.j.miller.8182@gmail.com>
2022-12-04 17:23:29 -0600

    day 3 part 1

---
+++ 2022/day_3/rust_solution/src/main.rs
@@ -0,0 +1,84 @@
+use std::{collections::HashSet, fs};
+
+fn get_priority(chr: char) -> u32 {
+    let chr = chr as u32;
+    match chr {
+        65..=90 => chr % 64 + 26,
+        97..=122 => chr % 96,
+        _ => 0,
+    }
+}
+
+fn get_common_in_components(first: &str, second: &str) -> char {
+    let first: HashSet<char> = first.chars().collect();
+    let second: HashSet<char> = second.chars().collect();
+    let intersection: HashSet<_> = first.intersection(&second).collect();
+    *intersection.iter().next().unwrap().clone()
+}
+
+fn split_string(line: &str) -> (String, String) {
+    let half = line.len() / 2;
+    let first: String = line.chars().take(half).collect();
+    let second: String = line.chars().rev().take(half).collect();
+    (first, second)
+}
+
+fn accumate_priority(contents: &str) -> u32 {
+    contents
+        .lines()
+        .map(|line| split_string(line))
+        .map(|tup| get_common_in_components(&tup.0, &tup.1))
+        .map(|chr| get_priority(chr))
+        .sum()
+}
+
+fn main() {
+    let contents = fs::read_to_string("../input").expect("Missing input file");
+    let priority_sum = accumate_priority(&contents);
+    println!("The sum of common priorities is {}", priority_sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_priority() {
+        assert_eq!(1, get_priority('a'));
+        assert_eq!(26, get_priority('z'));
+        assert_eq!(27, get_priority('A'));
+        assert_eq!(52, get_priority('Z'))
+    }
+
+    #[test]
+    fn test_get_common_in_components() {
+        assert_eq!(
+            'p',
+            get_common_in_components("vJrwpWtwJgWr", "hcsFMMfFFhFp")
+        )
+    }
+
+    #[test]
+    fn test_split_string() {
+        assert_eq!(
+            (
+                "vJrwpWtwJgWr".to_owned(),
+                "hcsFMMfFFhFp".chars().rev().collect::<String>()
+            ),
+            split_string("vJrwpWtwJgWrhcsFMMfFFhFp")
+        )
+    }
+
+    #[test]
+    fn test_accumate_priority() {
+        let contents = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(157, accumate_priority(contents))
+    }
+}
