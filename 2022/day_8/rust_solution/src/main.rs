4bd73b95c4f90adc3f546b351a7bce17df8c33be
Stuart Miller <stuart.j.miller.8182@gmail.com>
2022-12-10 10:29:09 -0600

    fmt

--- 2022/day_8/rust_solution/src/main.rs
+++ 2022/day_8/rust_solution/src/main.rs
@@ -1,13 +1,12 @@
-
 use std::{fs, vec};
 
 struct Matrix {
-    pub matrix: Vec<Vec<u32>>
+    pub matrix: Vec<Vec<u32>>,
 }
 
 impl Matrix {
     fn new(matrix: Vec<Vec<u32>>) -> Matrix {
-        Matrix{matrix}
+        Matrix { matrix }
     }
 
     fn n_rows(&self) -> usize {
@@ -19,7 +18,10 @@ impl Matrix {
     }
 
     fn get_column(&self, col: usize) -> Vec<u32> {
-        self.matrix.iter().map(|row| *row.get(col).unwrap()).collect()
+        self.matrix
+            .iter()
+            .map(|row| *row.get(col).unwrap())
+            .collect()
     }
 
     fn visible_to_left(&self, vector: &[u32], idx: usize) -> bool {
@@ -28,32 +30,35 @@ impl Matrix {
 
     fn score_to_left(&self, vector: &[u32], idx: usize) -> usize {
         match vector[0..idx].iter().rev().position(|n| n >= &vector[idx]) {
-            Some(position) => { position + 1 }
-            None => { idx }
+            Some(position) => position + 1,
+            None => idx,
         }
     }
 
     fn visible_to_right(&self, vector: &[u32], idx: usize) -> bool {
-        vector[idx + 1..vector.len()].iter().all(|n| n < &vector[idx])
+        vector[idx + 1..vector.len()]
+            .iter()
+            .all(|n| n < &vector[idx])
     }
 
     fn score_to_right(&self, vector: &[u32], idx: usize) -> usize {
-        match vector[idx + 1..vector.len()].iter().position(|n| n >= &vector[idx]) {
-            Some(position) => { (position + idx + 1) - idx}
-            None => { vector.len() - idx - 1}
+        match vector[idx + 1..vector.len()]
+            .iter()
+            .position(|n| n >= &vector[idx])
+        {
+            Some(position) => (position + idx + 1) - idx,
+            None => vector.len() - idx - 1,
         }
     }
 
     pub fn score(&self, row: usize, col: usize) -> usize {
         // score from row
         let vector = &self.matrix[row];
-        let row_score = self.score_to_left(&vector, col) 
-        * self.score_to_right(&vector, col);
+        let row_score = self.score_to_left(&vector, col) * self.score_to_right(&vector, col);
 
         // score from column
-        let vector =  self.get_column(col);
-        let col_score = self.score_to_left(&vector, row) 
-        * self.score_to_right(&vector, row);
+        let vector = self.get_column(col);
+        let col_score = self.score_to_left(&vector, row) * self.score_to_right(&vector, row);
 
         row_score * col_score
     }
@@ -117,13 +122,14 @@ mod tests {
 
     #[test]
     fn test_matrix_row_visibility() {
-        let matrix:Vec<Vec<u32>> = [
+        let matrix: Vec<Vec<u32>> = [
             [3, 0, 3, 7, 3].to_vec(),
-            [2,5,5,1,2].to_vec(),
-            [6,5,3,3,2].to_vec(),
-            [3,3,5,4,9].to_vec(),
-            [3,5,3,9,0].to_vec(),
-        ].to_vec();
+            [2, 5, 5, 1, 2].to_vec(),
+            [6, 5, 3, 3, 2].to_vec(),
+            [3, 3, 5, 4, 9].to_vec(),
+            [3, 5, 3, 9, 0].to_vec(),
+        ]
+        .to_vec();
 
         let matrix = Matrix::new(matrix);
 
@@ -142,18 +148,18 @@ mod tests {
         // 33549
         //    ^
         assert!(!matrix.is_visible_in_row(3, 1));
-
     }
 
     #[test]
     fn test_matrix_row_score() {
-        let matrix:Vec<Vec<u32>> = [
+        let matrix: Vec<Vec<u32>> = [
             [3, 0, 3, 7, 3].to_vec(),
-            [2,5,5,1,2].to_vec(),
-            [6,5,3,3,2].to_vec(),
-            [3,3,5,4,9].to_vec(),
-            [3,5,3,9,0].to_vec(),
-        ].to_vec();
+            [2, 5, 5, 1, 2].to_vec(),
+            [6, 5, 3, 3, 2].to_vec(),
+            [3, 3, 5, 4, 9].to_vec(),
+            [3, 5, 3, 9, 0].to_vec(),
+        ]
+        .to_vec();
 
         let matrix = Matrix::new(matrix);
 
@@ -163,14 +169,11 @@ mod tests {
         let row = &matrix.matrix[3];
         assert_eq!(2, matrix.score_to_left(&row, 2));
 
-
         let row = &matrix.matrix[1];
         assert_eq!(2, matrix.score_to_right(&row, 2));
 
         let row = &matrix.matrix[3];
         assert_eq!(2, matrix.score_to_right(&row, 2));
-
-
 
         let row = &matrix.matrix[4];
         assert_eq!(1, matrix.score_to_left(&row, 2));
@@ -178,98 +181,85 @@ mod tests {
         let row = &matrix.matrix[4];
         assert_eq!(1, matrix.score_to_right(&row, 2));
 
-
-
         let row = &matrix.matrix[2];
         assert_eq!(1, matrix.score_to_left(&row, 1));
 
         let row = &matrix.matrix[2];
         assert_eq!(3, matrix.score_to_right(&row, 1));
 
-
-
         let row = &matrix.matrix[4];
         assert_eq!(1, matrix.score_to_left(&row, 1));
 
         let row = &matrix.matrix[4];
         assert_eq!(2, matrix.score_to_right(&row, 1));
-
     }
 
     #[test]
     fn test_matrix_score() {
-        let matrix:Vec<Vec<u32>> = [
+        let matrix: Vec<Vec<u32>> = [
             [3, 0, 3, 7, 3].to_vec(),
-            [2,5,5,1,2].to_vec(),
-            [6,5,3,3,2].to_vec(),
-            [3,3,5,4,9].to_vec(),
-            [3,5,3,9,0].to_vec(),
-        ].to_vec();
+            [2, 5, 5, 1, 2].to_vec(),
+            [6, 5, 3, 3, 2].to_vec(),
+            [3, 3, 5, 4, 9].to_vec(),
+            [3, 5, 3, 9, 0].to_vec(),
+        ]
+        .to_vec();
 
         let matrix = Matrix::new(matrix);
 
-        assert_eq!(
-            4,
-            matrix.score(1, 2)
-        );
+        assert_eq!(4, matrix.score(1, 2));
 
-        assert_eq!(
-            8,
-            matrix.score(3, 2)
-        );
-    
+        assert_eq!(8, matrix.score(3, 2));
     }
 
     #[test]
     fn test_matrix_get_column() {
-        let matrix:Vec<Vec<u32>> = [
+        let matrix: Vec<Vec<u32>> = [
             [3, 0, 3, 7, 3].to_vec(),
-            [2,5,5,1,2].to_vec(),
-            [6,5,3,3,2].to_vec(),
-            [3,3,5,4,9].to_vec(),
-            [3,5,3,9,0].to_vec(),
-        ].to_vec();
+            [2, 5, 5, 1, 2].to_vec(),
+            [6, 5, 3, 3, 2].to_vec(),
+            [3, 3, 5, 4, 9].to_vec(),
+            [3, 5, 3, 9, 0].to_vec(),
+        ]
+        .to_vec();
 
         let matrix = Matrix::new(matrix);
 
-        assert_eq!(
-            [3,2,6,3,3].to_vec(),
-            matrix.get_column(0)
-        )
+        assert_eq!([3, 2, 6, 3, 3].to_vec(), matrix.get_column(0))
     }
 
     #[test]
     fn test_matrix_column_visibility() {
-        let matrix:Vec<Vec<u32>> = [
+        let matrix: Vec<Vec<u32>> = [
             [3, 0, 3, 7, 3].to_vec(),
-            [2,5,5,1,2].to_vec(),
-            [6,5,3,3,2].to_vec(),
-            [3,3,5,4,9].to_vec(),
-            [3,5,3,9,0].to_vec(),
-        ].to_vec();
+            [2, 5, 5, 1, 2].to_vec(),
+            [6, 5, 3, 3, 2].to_vec(),
+            [3, 3, 5, 4, 9].to_vec(),
+            [3, 5, 3, 9, 0].to_vec(),
+        ]
+        .to_vec();
 
         let matrix = Matrix::new(matrix);
 
-        //   3 
+        //   3
         //   2
-        // > 6 
+        // > 6
         //   3
         //   3
         assert!(matrix.is_visible_in_col(2, 0));
 
-        //   3 
+        //   3
         // > 2
-        //   6 
+        //   6
         //   3
         //   3
         assert!(!matrix.is_visible_in_col(1, 0));
 
-        //   3 
+        //   3
         //   5
-        // > 3 
+        // > 3
         //   5
         //   3
         assert!(!matrix.is_visible_in_col(3, 3));
-
     }
 }
